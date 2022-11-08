da97e127783ce875a2ed54386943a5058d57f633
Priya Patel <priyapatel@Priyas-MacBook-Air.local>
2022-11-08 21:49:54 +0530

    initial commit

---
+++ rest-web/src/main/java/com/doordash/merchant/campaign/MerchantCampaignApplication.java
@@ -0,0 +1,15 @@
+package com.doordash.merchant.campaign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantCampaignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MerchantCampaignApplication.class, args);
+
+        System.out.println("Hello World!!");
+    }
+
+}
